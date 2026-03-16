0acf61b3d57c981bf4d34b80fa4f00bd5832cb37
felipe-santos <felipe.santos@onebrain.com.br>
2026-03-16 16:25:16 -0300

    fix: resolve response entity for rest methods

--- src/main/java/com/challenge/coupon/controller/CouponAPI.java
+++ src/main/java/com/challenge/coupon/controller/CouponAPI.java
@@ -4,14 +4,13 @@ import com.challenge.coupon.dto.CouponRequest;
 import com.challenge.coupon.dto.CouponResponse;
 import com.challenge.coupon.dto.CouponDetailResponse;
 import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.UUID;
 
@@ -19,14 +18,11 @@ import java.util.UUID;
 public interface CouponAPI {
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    CouponResponse create(@RequestBody @Valid CouponRequest request);
+    ResponseEntity<CouponResponse> create(@RequestBody @Valid CouponRequest request);
 
     @GetMapping("/{id}")
-    @ResponseStatus(HttpStatus.OK)
-    CouponDetailResponse couponDetail(@PathVariable UUID id);
+    ResponseEntity<CouponDetailResponse> couponDetail(@PathVariable UUID id);
 
     @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    void delete(@PathVariable UUID id);
+    ResponseEntity<Void> delete(@PathVariable UUID id);
 }
