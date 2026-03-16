6f3f64642f60e3cf1d447e8984c1eb9c51770ca9
felipe-santos <felipe.santos@onebrain.com.br>
2026-03-16 10:00:18 -0300

    fix: Change CouponResponse to be a record

--- src/main/java/com/challenge/coupon/dto/CouponResponse.java
+++ src/main/java/com/challenge/coupon/dto/CouponResponse.java
@@ -2,24 +2,14 @@ package com.challenge.coupon.dto;
 
 import com.challenge.coupon.domain.Coupon;
 import lombok.Builder;
-import lombok.Value;
-import lombok.extern.jackson.Jacksonized;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
-@Value
 @Builder
-@Jacksonized
-public class CouponResponse {
-    UUID id;
-    String code;
-    String description;
-    BigDecimal discountValue;
-    LocalDate expirationDate;
-    boolean published;
-
+public record CouponResponse(UUID id, String code, String description, BigDecimal discountValue,
+                             LocalDate expirationDate, boolean published) {
     public static CouponResponse from(Coupon coupon) {
         return CouponResponse.builder()
                 .id(coupon.getId())
